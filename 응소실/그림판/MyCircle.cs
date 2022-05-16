55a24e94bc5c7a31f8144b2e508df8192a4b27a4
HyoungSeok Kim <59719632+Dy-tto@users.noreply.github.com>
2022-05-16 11:02:28 +0900

    Add files via upload

---
+++ 응소실/그림판/MyCircle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Collections;
+namespace 그림판
+{
+    public class MyCircle
+    {
+        private Rectangle rectC;
+        private int thick;
+        private bool isSolid;
+
+        public MyCircle()
+        {
+            rectC = new Rectangle();
+            thick = 1;
+            isSolid = true;
+        }
+
+        public void setRectC(Point start, Point finish,int thick,bool isSolid)
+        {
+            rectC.X = Math.Min(start.X, finish.X);
+            rectC.Y = Math.Min(start.Y, finish.Y);
+            rectC.Width = Math.Abs(start.X - finish.X);
+            rectC.Height = Math.Abs(start.Y - finish.Y);
+            this.thick = thick;
+            this.isSolid = isSolid;
+        }
+
+        public Rectangle getRectC()
+        {
+            return rectC;
+        }
+        public int getThick()
+        {
+            return thick;
+        }
+        public bool getSolid()
+        {
+            return isSolid;
+        }
+    }
+}
---
+++ 응소실/그림판/MyLines.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Collections;
+namespace 그림판
+{
+    public class MyLines
+    {
+        private Point[] point = new Point[2];
+        private int thick;
+        private bool isSolid;
+
+        public MyLines()
+        {
+            point[0] = new Point();
+            point[1] = new Point();
+            thick = 1;
+            isSolid = true;
+        }
+
+        public void setPoint(Point start, Point finish, int thick, bool isSolid)
+        {
+            point[0] = start;
+            point[1] = finish;
+            this.thick = thick;
+            this.isSolid = isSolid;
+        }
+        public Point getPoint1()
+        {
+            return point[0];
+        }
+        public Point getPoint2()
+        {
+            return point[1];
+        }
+        public int getThick()
+        {
+            return thick;
+        }
+        public bool getSolid()
+        {
+            return isSolid;
+        }
+    }
+}
